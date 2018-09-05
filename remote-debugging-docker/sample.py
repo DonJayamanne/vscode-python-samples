063f26e9cfe42b480ad005fc68d67b085566b1e5
Don Jayamanne <don.jayamanne@yahoo.com>
2018-09-05 16:12:57 -0700

    changes to support new debugger

--- remote-debugging-docker/sample.py
+++ remote-debugging-docker/sample.py
@@ -5,7 +5,7 @@ import os
 print("Waiting to attach")
 
 address = ('0.0.0.0', 3000)
-ptvsd.enable_attach('my_secret', address)
+ptvsd.enable_attach(address)
 ptvsd.wait_for_attach()
 
 time.sleep(2)
